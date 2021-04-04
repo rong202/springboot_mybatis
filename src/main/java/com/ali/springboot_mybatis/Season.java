38357c3f68a2196f6287f008511f91c03adb0625
rong202 <fei202s@163.com>
2021-04-04 21:54:04 +0800

    新增redisConfigrution,RedisTest

---
+++ src/main/java/com/ali/springboot_mybatis/Season.java
@@ -0,0 +1,31 @@
+package com.ali.springboot_mybatis;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public enum Season {
+    // 定义四个枚举实例
+    SPRING(1,"春"),SUMMER(2,"夏"),AUTUMN(3,"秋"),WINTER(4,"冬");
+
+    private  int id;
+    private  String name;
+
+ /*   public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+*/
+
+
+    public static void main(String[] args) {
+        System.out.println(Season.SPRING.name);
+        System.out.println(Season.SUMMER.name);
+        System.out.println(Season.AUTUMN.name);
+        System.out.println(Season.WINTER.name);
+    }
+}
